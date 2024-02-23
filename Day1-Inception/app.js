07134aebdd99501a1fde2678c2c927bad8ee6f3b
Tridib11 <tridibghosh12345@gmail.com>
2024-02-24 03:15:37 +0530

    files added

--- Day1-Inception/app.js
+++ Day1-Inception/app.js
@@ -1,4 +1,9 @@
-const heading = React.createElement("h1", {}, "Hello world from react!");
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },//Attribute
+  "Hello world from react!"
+);
+console.log(heading);
 //everythinng that we will do we will do inside the root so ReactDOM.createRoot()
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
