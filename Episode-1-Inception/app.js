3a91aa86180fd1e9d7b129b2cfe6772da9578ab9
Tridib11 <tridibghosh12345@gmail.com>
2024-02-24 03:35:36 +0530

    Files Modified

--- Episode-1-Inception/app.js
+++ Episode-1-Inception/app.js
@@ -1,9 +1,33 @@
-const heading = React.createElement(
-  "h1",
-  { id: "heading" },//Attribute
-  "Hello world from react!"
-);
-console.log(heading);
+//Replicating this in React
+/*
+    <div id="parent">
+      <div id="child">
+        <h1>I am an h1 tag</h1>
+        <h2>I am an h2 tag</h2>       
+      </div>
+      <div id="child2">
+        <h1>I am an h1 tag</h1>
+        <h2>I am an h2 tag</h2>       
+      </div>
+    </div>
+*/
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I am an h1 tag"),
+    React.createElement("h2", {}, "I am an h2 tag"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "I am an h1 tag"),
+    React.createElement("h2", {}, "I am an h2 tag"),
+  ]),
+]);
+
+// const heading = React.createElement(
+//   "h1",
+//   { id: "heading" }, //Attribute
+//   "Hello world from react!"
+// );
+console.log(parent);
 //everythinng that we will do we will do inside the root so ReactDOM.createRoot()
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(parent);
