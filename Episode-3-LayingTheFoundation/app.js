5be77d07b6e6af233df486cb7bb050cc03c895d5
Tridib11 <tridibghosh12345@gmail.com>
2024-02-25 18:42:10 +0530

    File Modified

--- Episode-3-LayingTheFoundation/app.js
+++ Episode-3-LayingTheFoundation/app.js
@@ -4,8 +4,9 @@ import ReactDOM from "react-dom/client";
 //JSX - HTML-like or XML-like syntax
 
 //JSX(transpiled before it reaches the JS engine) - PARCEL - Babel
-const jsxHeading = <h1 id="Heading" className="head">Namaste React using JS</h1>;
 
+//Component
+const TitleComponent = () => <h1 className="head">Namaste React using JS</h1>;
 
 //React component
 // -> Class Based Component - OLD
@@ -13,10 +14,23 @@ const jsxHeading = <h1 id="Heading" className="head">Namaste React using JS</h1>
 
 //React Functional Component
 
-const HeadingComponent=()=>{
-  return <h1>Namaste React Functional Component</h1>
-}
+const number=1000
+
+const HeadingComponent = () => (
+  //This is component composition - Composing two Component into one Aother
+  <div id="container">
+    {100+200}
+    <h1 className="heading">Namaste React Functional Component</h1>
+  </div>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(jsxHeading);
+root.render(<HeadingComponent />);
+
+// root.render(jsxHeading);
+
+/*
+way to render a functional component
+root.render(<HeadingComponent />);
+*/
