327ad054d658e6aac1c2fda546b1e91af932d770
Tridib11 <tridibghosh12345@gmail.com>
2024-02-26 06:05:09 +0530

    Files added

--- Food Ordering App/Food Ordeing Website/src/main.jsx
+++ Food Ordering App/Food Ordeing Website/src/main.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 /**
  * Header
@@ -16,5 +16,34 @@ import ReactDOM from 'react-dom/client'
  *  - Contact
  */
 
-const root=ReactDOM.createRoot(document.getElementById("root"))
-root.render()
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png"
+          alt=""
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart 🛒</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header></Header>
+    </div>
+  );
+};
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppLayout />);
