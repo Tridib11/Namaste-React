b0af812c22ef77e3e9a8d38f3d0f6d3e9de68f8d
Tridib11 <tridibghosh12345@gmail.com>
2024-02-26 05:41:33 +0530

    File Added

---
+++ Food Ordering App/Food Ordeing Website/src/main.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+/**
+ * Header
+ *  - Logo
+ *  - Nav Items
+ * Body
+ *  - Search
+ *  - ResturantContainer
+ *    - ResturantCard
+ * Footer
+ *  - Copyright
+ *  - Links
+ *  - Address
+ *  - Contact
+ */
+
+const root=ReactDOM.createRoot(document.getElementById("root"))
+root.render()
