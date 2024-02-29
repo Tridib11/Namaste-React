38b11dce8a10a53525ca205af108589849b8070a
Tridib11 <tridibghosh12345@gmail.com>
2024-03-01 01:19:00 +0530

    MVC Added

--- Food-odering-app-rebuild/src/main.jsx
+++ Food-odering-app-rebuild/src/main.jsx
@@ -3,29 +3,7 @@ import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
 
-const ResturantCard = (props) => {
-  const { resData } = props;
-  const { name, cuisines, costForTwo, avgRating, cloudinaryImageId } =
-    resData?.info;
-  const { slaString } = resData?.info.sla;
-  return (
-    <div className="res-card" style={{ backgroundColor: "#f0f0f0" }}>
-      <img
-        className="resturant-logo"
-        src={
-          "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" +
-          cloudinaryImageId
-        }
-        alt=""
-      />
-      <h3>{name}</h3>
-      <h4>{cuisines.join(", ")}</h4>
-      <h4>{costForTwo}</h4>
-      <h4>{avgRating}⭐</h4>
-      <h4>{slaString}</h4>
-    </div>
-  );
-};
+
 const resList = [
   {
     info: {
