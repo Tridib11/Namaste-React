505073ce1cf64e28ca1a182e10312ce233b233df
Tridib11 <tridibghosh12345@gmail.com>
2024-03-01 01:16:32 +0530

    MVC Added

---
+++ Food-odering-app-rebuild/src/components/Body.jsx
@@ -0,0 +1,14 @@
+const Body = () => {
+    return (
+      <div className="body">
+        <div className="search">Search</div>
+        <div className="resturant-container">
+          {resList.map((resturant) => (
+            <ResturantCard key={resturant.info.id} resData={resturant} />
+          ))}
+        </div>
+      </div>
+    );
+  };
+
+  export default Body;
