7b6264c6b4b4bb2ffb031ab6db86a403469fa538
Tridib11 <tridibghosh12345@gmail.com>
2024-03-01 02:18:23 +0530

    MVC Added

--- Food-odering-app-rebuild/src/components/Body.jsx
+++ Food-odering-app-rebuild/src/components/Body.jsx
@@ -1,16 +1,20 @@
 import ResturantCard from "./ResturantCard";
 import resList from "../utils/mockData";
 const Body = () => {
-    return (
-      <div className="body">
-        <div className="search">Search</div>
-        <div className="resturant-container">
-          {resList.map((resturant) => (
-            <ResturantCard key={resturant.info.id} resData={resturant} />
-          ))}
-        </div>
+  return (
+    <div className="body">
+      <div className="filter">
+        <button onClick={()=>{
+          console.log("clicked")
+        }} className="filter-btn">Top Rated Resturants</button>
       </div>
-    );
-  };
+      <div className="resturant-container">
+        {resList.map((resturant) => (
+          <ResturantCard key={resturant.info.id} resData={resturant} />
+        ))} 
+      </div>
+    </div>
+  );
+};
 
-  export default Body;
+export default Body;
