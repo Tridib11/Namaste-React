5e28b7b50c6c8fdad6f092d4c0a99fcfce39d445
Tridib11 <tridibghosh12345@gmail.com>
2024-02-29 16:46:03 +0530

    File Modified

---
+++ Food-odering-app-rebuild/src/main.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png"
+          alt=""
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home 🏠</li>
+          <li>About Us 👤</li>
+          <li>Contact Us 📞</li>
+          <li>Cart 🛒</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+const AppLayout = () => {
+  return <div className="app">
+    <Header></Header>
+  </div>;
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppLayout/>);
