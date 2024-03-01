5a376f20495930f9443a37d2c82cd096aa71fe2b
Tridib11 <tridibghosh12345@gmail.com>
2024-03-02 03:44:34 +0530

    File Modified

--- Food-odering-app-rebuild/src/components/Body.jsx
+++ Food-odering-app-rebuild/src/components/Body.jsx
@@ -6,17 +6,17 @@ const Body = () => {
   const [listOfResturants, setListOfResturants] = useState(resList);
 
   useEffect(() => {
-    fetchData()
+    fetchData();
   }, []);
 
-  const fetchData=async ()=>{
-    const data=await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.89960&lng=80.22090&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING")
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.89960&lng=80.22090&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
 
-    const json=data.json()
+    const json = data.json();
     console.log(json);
-
-  }
-
+  };
 
   return (
     <div className="body">
